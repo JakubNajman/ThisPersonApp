8b1ce83418536b18e91bd2c1fcaf2fe878304969
JakubNajman <01149568@pw.edu.pl>
2024-07-10 14:06:52 +0200

    tidy up react and add bootstrap navbar with axios req for images from django

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Navbar, Container, Row, Col, Image } from 'react-bootstrap';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const promises = [];
+      for (let i = 1; i <= 20; i++) {
+        promises.push(
+          axios.get(`/gallery/preview/${i}`, { responseType: 'arraybuffer' })
+            .then(response => {
+              const imageUrl = URL.createObjectURL(new Blob([response.data], { type: 'image/jpeg' }));
+              return imageUrl;
+            })
+            .catch(() => null)
+        );
+      }
+      const images = await Promise.all(promises);
+      setImages(images.filter(image => image !== null));
+    };
+
+    fetchImages();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ThisPersonApp</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container className="mt-4">
+        <Row>
+          {images.map((image, index) => (
+            <Col key={index} xs={6} md={4} lg={3} className="mb-4">
+              <Image src={image} rounded width="200" height="200" />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
